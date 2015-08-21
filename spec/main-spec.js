13f92fe0b758843002a53d5530fc64e697d8999c
Li Tongxin <li.tongxin@rea-group.com>
2015-08-21 18:06:05 +0800

    Change karma config to run test.

---
+++ spec/main-spec.js
@@ -0,0 +1,27 @@
+describe('main', function(){
+  'use strict';
+
+  beforeEach(function() {
+    jasmine.getFixtures().fixturesPath = 'base/spec/fixtures';
+    loadFixtures('myfixture.html');
+    jasmine.Ajax.install();
+    jasmine.Ajax.stubRequest('http://location-backend-service.herokuapp.com/locations?name=mel')
+                .andReturn({
+                    responseText: 'location response'
+                });
+  });
+
+  afterEach(function() {
+       jasmine.Ajax.uninstall();
+  });
+
+  it('should send ajax call when click searchButton', function(){
+    var locationInput = $('#locationInput');
+    spyOn(locationInput, 'val').and.returnValue('mel');
+    spyOnEvent($('#searchButton'), 'click');
+    spyOn($, 'ajax');
+    $('#searchButton').click();
+    expect('click').toHaveBeenTriggeredOn('#searchButton');
+    expect($.ajax.calls.mostRecent().url).toEqual('http://location-backend-service.herokuapp.com/locations?name=mel');
+  });
+});
