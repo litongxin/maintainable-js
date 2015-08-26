621181f4472b4c70e207d3d52048750237ccad37
Ma Libin <libin.ma@rea-group.com>
2015-08-26 09:02:49 +0800

    refator continue click like button

--- src/js/view/result-items-view.js
+++ src/js/view/result-items-view.js
@@ -17,18 +17,14 @@ var ResultItemsView = Backbone.View.extend({
     var id = $(e.target).parent().find('span').text();
     var likes = that.likeModel.get('likes');
     var results = that.resultModel.get('results');
-    var alreadyInLikedPlaces = _.find($('#likedPlaces li span'), function(list) { return list.textContent == item});
-    if(!alreadyInLikedPlaces){
-      likes.push({"name": item, "resultId": id});
-      this.likeModel.trigger('change:likes', likes);
-      $(e.target).disable = true;
-      _.each(results,function(result){
-        if(result.id == id){
-          result.like = true;
-        }
-      });
-      this.resultModel.trigger('change:results', results);
-    }
+    likes.push({"name": item, "resultId": id});
+    this.likeModel.trigger('change:likes', likes);
+    _.each(results,function(result){
+      if(result.id == id){
+        result.like = true;
+      }
+    });
+    this.resultModel.trigger('change:results', results);
   },
 
   el: '#resultItems',
