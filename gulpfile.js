bb55211cbf9d63abe82fea79eab5e14879bc9b4a
Li Tongxin <li.tongxin@rea-group.com>
2015-09-15 20:03:39 +0800

    Add fingerprint for the css/js file and compile html.

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,19 @@ var gulp = require('gulp'),
     jshint = require('gulp-jshint'),
     browserify = require('browserify'),
     source = require('vinyl-source-stream'),
-    KarmaServer = require('karma').Server;
+    KarmaServer = require('karma').Server,
+    rev = require('gulp-rev'),
+    fs = require('fs'),
+    handlebars = require('gulp-compile-handlebars');
+    rename = require('gulp-rename'),
+    handlebarOpts = {
+      helpers: {
+          assetPath: function (path, context) {
+              return ['build/assets', context.data.root[path]].join('/');
+          }
+      }
+    },
+    clean = require('gulp-clean');
 
 gulp.task('test', function(done){
   new KarmaServer({
@@ -10,7 +22,7 @@ gulp.task('test', function(done){
   }, done).start();
 });
 
-gulp.task('js', function() {
+gulp.task('js', ['cleanup'], function() {
     return gulp.src(['src/*.js', 'spec/*.js'])
         .pipe(jshint('.jshintrc'))
         .pipe(jshint.reporter('jshint-stylish'));
@@ -23,7 +35,7 @@ gulp.task('js', function() {
 //     .pipe(gulp.dest('build/js'));
 // });
 
-gulp.task('browserify', function() {
+gulp.task('browserify', ['js'] ,function() {
     return browserify('src/js/componentize.js')
         .bundle()
         .on('error', function(err){
@@ -31,12 +43,34 @@ gulp.task('browserify', function() {
           this.emit('end');
         })
         .pipe(source('bundle.js'))
-        .pipe(gulp.dest('build/js'));
+        .pipe(gulp.dest('src/js'));
 });
 
 gulp.task('watch', function() {
   gulp.watch('src/js/**/*', ['browserify']);
 });
 
+gulp.task('rev', ['browserify'] ,function () {
+  return gulp.src(['src/css/*.css', 'src/js/bundle.js'], { base: 'src'})
+     .pipe(gulp.dest('build/assets'))
+     .pipe(rev())
+     .pipe(gulp.dest('build/assets'))
+     .pipe(rev.manifest())
+     .pipe(gulp.dest('build/assets'));
+});
 
-gulp.task('default', ['js', 'test']);
+gulp.task('compileHtml', ['rev'], function () {
+    var manifest = JSON.parse(fs.readFileSync('build/assets/rev-manifest.json', 'utf8'));
+
+    return gulp.src('six.hbs')
+        .pipe(handlebars(manifest, handlebarOpts))
+        .pipe(rename('six.html'))
+        .pipe(gulp.dest(''));
+});
+
+gulp.task('cleanup', function(){
+  return gulp.src(['build', 'public'], {read: false})
+        .pipe(clean());
+});
+
+gulp.task('default', ['compileHtml']);
